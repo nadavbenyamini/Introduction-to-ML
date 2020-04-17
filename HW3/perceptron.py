b98d0e567dd6e915f5709e25817e96554939a8a4
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-04-17 22:21:00 +0300

    HW3 Finished

--- HW3/perceptron.py
+++ HW3/perceptron.py
@@ -54,15 +54,9 @@ def get_n_samples(data, labels, n):
     return n_data, n_labels
 
 
-def sign(x):
-    if x == 0:
-        return 0
-    return 1 if x > 0 else -1
-
-
 def predict(w, x):
     dp = np.dot(w, x)
-    return sign(dp.astype(float))
+    return 1 if dp.astype(float) >= 0 else -1
 
 
 def perceptron(data, labels):
@@ -79,7 +73,7 @@ def perceptron(data, labels):
 
 def calc_accuracy(w, data, labels):
     n = len(labels)
-    return sum([predict(w, data[i]) == labels[i] for i in range(n)]) / n
+    return sum(predict(w, data[i]) == labels[i] for i in range(n)) / n
 
 
 def display_table(rows, columns):
@@ -93,25 +87,34 @@ def display_table(rows, columns):
 def main():
     train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper()
     n_list = [5, 10, 50, 100, 500, 1000, 5000]
-    accuracies = {}
+    acc = {}
     for n in n_list:
-        accuracies[n] = []
-        for j in range(10):
+        acc[n] = []
+        for j in range(100):
             train_data_n, train_labels_n = get_n_samples(train_data, train_labels, n)
             w = perceptron(train_data_n, train_labels_n)
-            accuracies[n].append(calc_accuracy(w, test_data, test_labels))
-        print('n = {}, mean_accuracy = {}, median_accuracy = {}'.format(n, np.mean(accuracies[n]), np.median(accuracies[n])))
-
-    c = ['N', 'Mean Accuracy', '95% Percentile', '5% Percentile']
-    r = []
-    for n in accuracies:
-        r += [[n, np.mean(accuracies[n]), np.percentile(accuracies[n], q=0.95),  np.percentile(accuracies[n], q=0.05)]]
+            acc[n].append(calc_accuracy(w, test_data, test_labels))
 
     # Q1:
-    display_table(r, c)
+    columns = ['N', 'Mean Accuracy', '5% Percentile', '95% Percentile']
+    rows = [[n, np.mean(acc[n]), np.percentile(acc[n], q=5),  np.percentile(acc[n], q=95)] for n in n_list]
+    display_table(rows, columns)
 
     # Q2:
+    w = perceptron(train_data, train_labels)
     plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
+    plt.show()  # TODO - Comment out
+
+    # Q3:
+    print('Accuracy = {}'.format(calc_accuracy(w, test_data, test_labels)))
+
+    # Q4:
+    for i in range(len(test_labels)):
+        prediction = predict(w, test_data[i])
+        if prediction != test_labels[i]:
+            plt.imshow(np.reshape(test_data[i], (28, 28)), interpolation='nearest')
+            plt.show()  # TODO - Comment out
+            break
 
 
 if __name__ == '__main__':
