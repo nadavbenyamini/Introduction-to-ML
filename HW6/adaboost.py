87b200082712ec740491969178c3ff1a781ddeaf
nadavbenyamini <nadav.benyamini6@gmail.com>
2020-06-03 17:48:38 +0300

    Q2, Q3

--- HW6/adaboost.py
+++ HW6/adaboost.py
@@ -9,22 +9,26 @@ from process_data import parse_data
 
 np.random.seed(7)
 
-MARKED_DIMS = set()
-
 
 def sign(x):
     return 1 if x >= 0 else -1
 
 
+# h is a tuple of theta and j, this is an activation of the implied classifier on a sample x[i]
+def activate_h(h, x, i):
+    theta, j = h
+    return sign(theta - x[i][j])
+
+
 def update_D(D, ht, X_train, y_train, wt):
     n = len(y_train)
-    sum_D = sum(D[i] * np.exp(-wt * y_train[i] * ht(X_train, i)) for i in range(n))
-    return [(D[i] * np.exp(-wt * y_train[i] * ht(X_train, i)))/sum_D for i in range(n)]
+    sum_D = sum(D[i] * np.exp(-wt * y_train[i] * activate_h(ht, X_train, i)) for i in range(n))
+    return [(D[i] * np.exp(-wt * y_train[i] * activate_h(ht, X_train, i)))/sum_D for i in range(n)]
 
 
 def update_e(D, ht, X_train, y_train):
     n = len(y_train)
-    return sum(D[i] * int(ht(X_train, i) != y_train[i]) for i in range(n))
+    return sum(D[i] * int(activate_h(ht, X_train, i) != y_train[i]) for i in range(n))
 
 
 def update_w(et):
@@ -38,17 +42,9 @@ def get_weak_learner(D, X_train, y_train):
     best_theta = 0
     best_direction = 1
     best_f = np.inf
-    threshold = 0.01
-
-    # Some randomness to make it interesting
-    indices = list(range(m))
-    np.random.shuffle(indices)
 
     # Finding the best theta for each j, then finding best j
-    for j in indices:
-
-        # if j in MARKED_DIMS:  # Don't go for the same j twice
-        #     continue
+    for j in range(m):
 
         # Sorting x and y together by x[j]
         train_tuples = sorted([(X_train[i][j], y_train[i], D[i]) for i in range(n)], key=lambda tup: (tup[0], np.random.rand()))
@@ -67,7 +63,6 @@ def get_weak_learner(D, X_train, y_train):
         for i in range(n):
             direction = 1
             f = f - y_train_j[i]*D_j[i]
-            # print(j, f, i, y_train_j[i], D_j[i])
             if f < 0:
                 print(j, f, i, y_train_j[i], D_j[i], f + y_train_j[i]*D_j[i])
                 print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
@@ -77,10 +72,9 @@ def get_weak_learner(D, X_train, y_train):
                 best_theta = (X_train_j[i] + X_train_j[i + 1])/2
                 best_j = j
                 best_direction = direction
-                print('Best h found: j={}, f={}, theta={}, direction={}'.format(best_j, best_f, best_theta, best_direction))
+                # print('Best h found: j={}, f={}, theta={}, direction={}'.format(best_j, best_f, best_theta, best_direction))
 
-    MARKED_DIMS.add(best_j)
-    return lambda x, i: sign((best_theta - x[i][best_j])) * best_direction
+    return best_theta, best_j
 
 
 def run_adaboost(X_train, y_train, T):
@@ -114,30 +108,53 @@ def run_adaboost(X_train, y_train, T):
     return hypotheses, alpha_vals
 
 
-def main():
-    print('Started')
-    data = parse_data()
-    if not data:
-        return
+def question1(data):
+    print('Started Q1')
+
+    def loss(X, y, hypotheses, alpha_vals):
+        n = len(y)
+        t = len(hypotheses)
+        pred = [sign(sum(alpha_vals[k] * (activate_h(hypotheses[k], X, i)) for k in range(t))) for i in range(n)]
+        return sum(pred[i] != y[i] for i in range(n)) / n
+    questions1_3(data, loss)
+
+
+def question2(data):
+    T = 10
     (X_train, y_train, X_testt, y_testt, vocab) = data
-    T = 20
+    hypotheses, alpha_vals = run_adaboost(X_train, y_train, T)
+
+    print(hypotheses)
+
+
+def question3(data):
+    print('Started Q3')
+
+    def loss(X, y, hypotheses, alpha_vals):
+        n = len(y)
+        t = len(hypotheses)
+        l = 0
+        for i in range(n):
+            s = sum(alpha_vals[k] * (activate_h(hypotheses[k], X, i)) for k in range(t))
+            l += np.exp(-y[i] * s)
+        return s / n
+    questions1_3(data, loss)
+
+
+def questions1_3(data, loss):
+    T = 10
+    (X_train, y_train, X_testt, y_testt, vocab) = data
     hypotheses, alpha_vals = run_adaboost(X_train, y_train, T)
 
     train_errors = []
     testt_errors = []
 
-    n_testt = len(y_testt)
-    n_train = len(y_train)
-
     print('\nSummary:')
     for t in range(len(hypotheses)):
-        train_pred = [sign(sum(alpha_vals[j] * (hypotheses[j](X_train, i)) for j in range(t))) for i in range(n_train)]
-        testt_pred = [sign(sum(alpha_vals[j] * (hypotheses[j](X_testt, i)) for j in range(t))) for i in range(n_testt)]
-
-        train_errors.append(sum(train_pred[i] != y_train[i] for i in range(n_train)) / n_train)
-        testt_errors.append(sum(testt_pred[i] != y_testt[i] for i in range(n_testt)) / n_testt)
-
-        print('~~~ Iteration #{}, Train Error = {}, Test Error = {} ~~~'.format(t, train_errors[-1], testt_errors[-1]))
+        hypos = hypotheses[:t]
+        alphas = alpha_vals[:t]
+        train_errors.append(loss(X_train, y_train, hypos, alphas))
+        testt_errors.append(loss(X_testt, y_testt, hypos, alphas))
 
     plt.plot(train_errors, color="blue", label='Train')
     plt.plot(testt_errors, color="red", label='Test')
@@ -146,6 +163,15 @@ def main():
     plt.xticks(list(round(x) for x in range(len(hypotheses))))
     plt.ylabel('Error')
     plt.show()
+
+
+def main():
+    print('Started')
+    data = parse_data()
+    if not data:
+        return
+    # question1(data)
+    question2(data)
 
 
 if __name__ == '__main__':
